c01078f3306cde8851dc65a4e572aadb9df2cd90
gmelum <gmelum@my.com>
2022-06-03 02:40:11 +0300

    main

--- src/engine/hooks/useCallbackState.ts
+++ src/engine/hooks/useCallbackState.ts
@@ -1,6 +1,7 @@
 import { useRecoilCallback, RecoilState, SetterOrUpdater, useSetRecoilState } from 'recoil';
 
-const useCallbackState = <T extends any>(state: RecoilState<T>): [() => Promise<T>, SetterOrUpdater<T>] => 
-    [useRecoilCallback(({ snapshot }) => () => snapshot.getPromise(state)), useSetRecoilState(state)]
+const useCallbackState = <T extends any>(state: RecoilState<T>): [() => Promise<T>, SetterOrUpdater<T>] => {
+    return [useRecoilCallback(({ snapshot }) => () => snapshot.getPromise(state)), useSetRecoilState(state)]
+}
 
 export default useCallbackState;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,6 @@ import bridge from '@vkontakte/vk-bridge';
 import App from 'App';
 import './style.css';
 
-bridge.subscribe(async (e: any) => {
-    switch (e.detail.type) {
-        case "VKWebAppUpdateConfig": bridge.send("VKWebAppResizeWindow", { "width": 907, "height": e.detail.data.viewport_height - 145 });
-    }
-});
-
 ReactDOM.render(
     <RecoilRoot>
         <App />
